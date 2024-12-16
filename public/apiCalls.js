3c6590e9af84f3da9a902b49afaa42fda7377155
Antonio Dell'Arte <antonio.dellarte@external.eni.com>
2024-12-16 21:27:19 +0100

    ok

--- public/apiCalls.js
+++ public/apiCalls.js
@@ -1,9 +1,9 @@
-function obtainTokenForApi(){
+function obtainTokenForGraph(){
     // MSAL.js v2 exposes several account APIs, logic to determine which account to use is the responsibility of the developer
     const account = myMSALObj.getAllAccounts()[0];
 
     const accessTokenRequest = {
-    scopes: ["user.read", "api://2f2359cc-9457-4ae7-8412-4bad2d598214/AuthenticatedUser"],
+    scopes: ["user.read"],
     account: account,
     };
 
@@ -17,7 +17,7 @@ function obtainTokenForApi(){
         callGraphFromFE(accessToken);
     })
     .catch(function (error) {
-        document.getElementById('FieldResponseTable').innerText = error;
+        document.getElementById('FieldTableGraphResponse').innerText = error;
 
         console.log(error);
     });
@@ -25,7 +25,7 @@ function obtainTokenForApi(){
 
 function callGraphFromFE(accessToken){
     // Call the function to obtain an access token
-    document.getElementById('FieldTokenTable').innerText = accessToken;
+    document.getElementById('FieldTableGraphToken').innerText = accessToken;
 
     // Call the graph API /me endpoint
     fetch('https://graph.microsoft.com/v1.0/me', {
@@ -35,10 +35,61 @@ function callGraphFromFE(accessToken){
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('FieldResponseTable').innerText = JSON.stringify(data, null, 2);
+        document.getElementById('FieldTableGraphResponse').innerText = JSON.stringify(data, null, 2);
     })
     .catch(error => {
-        document.getElementById('FieldResponseTable').innerText = error;
+        document.getElementById('FieldTableGraphResponse').innerText = error;
+    });
+
+}
+
+
+function obtainTokenForBackend(){
+    // MSAL.js v2 exposes several account APIs, logic to determine which account to use is the responsibility of the developer
+    const account = myMSALObj.getAllAccounts()[0];
+
+    const accessTokenRequest = {
+    scopes: ["api://2f2359cc-9457-4ae7-8412-4bad2d598214/AuthenticatedUser"],
+    account: account,
+    };
+
+    myMSALObj
+    .acquireTokenSilent(accessTokenRequest)
+    .then(function (accessTokenResponse) {
+        // Acquire token silent success
+        let accessToken = accessTokenResponse.accessToken;
+
+        // Call your API with token
+        callBeFromFE(accessToken);
+    })
+    .catch(function (error) {
+        document.getElementById('FieldTableBeResponse').innerText = error;
+
+        console.log(error);
+    });
+}
+
+function callBeFromFE(accessToken){
+    // Call the function to obtain an access token
+    document.getElementById('FieldTableGraphToken').innerText = accessToken;
+
+    // Call the backend API /iam endpoint as post
+    
+    fetch('https://webapistaticapp.azure-api.net/iam', {
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer ' + accessToken
+        },
+        body: JSON.stringify({
+            "response": "This is a response from the backend API"
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('FieldTableBeResponse').innerText = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        document.getElementById('FieldTableBeResponse').innerText = error;
     });
 
 }
